e9d2e1ada794c2d07425746120d2930086126517
TomoyaWashio <148823993+TomoyaWashio@users.noreply.github.com>
2024-11-27 15:43:27 +0900

    初回コミット

---
+++ Program.cs
@@ -0,0 +1,36 @@
+using IgniteUI.Blazor.Controls;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddIgniteUIBlazor(
+    typeof(IgbDataChartCoreModule),
+    typeof(IgbCrosshairLayerModule),
+    typeof(IgbNumericXAxisModule),
+    typeof(IgbLineSeriesModule),
+    typeof(IgbDataChartAnnotationModule),
+    typeof(IgbDataChartInteractivityModule)
+    );
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
